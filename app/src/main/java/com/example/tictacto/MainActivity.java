47ce6109499c41ff7a6a7dc985c15490d88f24f1
Rickert-Gonzales <rickertgonzales@gmail.com>
2022-03-06 23:44:54 -0500

    Segunda parte Logica del Juego

--- app/src/main/java/com/example/tictacto/MainActivity.java
+++ app/src/main/java/com/example/tictacto/MainActivity.java
@@ -3,8 +3,41 @@ package com.example.tictacto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 0 = yellow, 1 = red
+    int activePlayer = 0;
+
+    //bandera
+    boolean gameIsActive = true;
+
+    //Estados del juego: para validar la repetición
+    int[] gameState = {2,2,2,2,2,2,2,2,2};
+
+    //Al precionar el boton
+    public void dropIn(View view) {
+        ImageView counter = (ImageView) view;
+
+        //Capturamos el tag de las imagenes
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+        if (gameState[tappedCounter] == 2 && gameIsActive) {
+            gameState[tappedCounter] = activePlayer;
+
+            counter.setTranslationY(-1000f);
+
+            if(activePlayer == 0){
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
